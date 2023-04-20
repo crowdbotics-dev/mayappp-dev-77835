c6e1418983ac18fe833713c79991d14c70b35e77
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-04-20 12:20:18 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "mayappp-dev-77835",
   "url": "https://mayappp-dev-77835.botics.co",
   "api": "https://mayappp-dev-77835.botics.co/api/v1",
-  "initialRoute": "Untitled11"
+  "initialRoute": "Untitled6"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
